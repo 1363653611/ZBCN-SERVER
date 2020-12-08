c4b0a33e666db8843bccb53b5db2a50da966312f
zbcn <zbcn810@163.com>
2020-12-08 13:23:58 +0800

    hystrix-dashboard 监控服务

---
+++ zbcn-common/hystrix-dashboard/src/main/java/com/zbcn/hystrixdashboard/HystrixDashboardApplication.java
@@ -0,0 +1,17 @@
+package com.zbcn.hystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+@EnableHystrixDashboard //启动监控功能
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardApplication.class, args);
+    }
+
+}
---
+++ zbcn-common/hystrix-server/src/main/java/com/zbcn/hystrixserver/config/CommonConfig.java
@@ -0,0 +1,20 @@
+package com.zbcn.hystrixserver.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CommonConfig {
+    //解决问题：使用hystrix dashboard仪表盘时,Failed opening connection to http://localhost:8091/hystrix.stream?delay=100 : 404 : HTTP/1.1 404
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/actuator/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+}
